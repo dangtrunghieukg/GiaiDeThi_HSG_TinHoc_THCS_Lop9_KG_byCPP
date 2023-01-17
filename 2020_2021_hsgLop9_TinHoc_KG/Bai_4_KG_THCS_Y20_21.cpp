49d0dc2f5bf30b3c69bedf98b5eb14a5452b6f0d
dangtrunghieukg <dangtrunghieuspt@gmail.com>
2023-01-18 05:10:36 +0700

    Them bai 4,

---
+++ 2020_2021_hsgLop9_TinHoc_KG/Bai_4_KG_THCS_Y20_21.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "Nha tuoi bo x = "; cin >> x;
+    cout << "Nhap tuoi con y = "; cin >> y;
+    while (x-y<=20){
+        cout << "Yeu cau x - y > 20, nhap lai:" << endl;
+        cout << "x = "; cin >> x;
+        cout << "y = "; cin >> y;
+    }
+    int tuoiBo = x;
+    int tuoiCon = y;
+    if (x==2*y){
+        cout << "Hien nay tuoi con bang 1/2 tuoi bo.";
+    }
+    else{
+        int demNguoc = 0;
+        int demXuoi = 0;
+        bool demNguocOk = false;
+        while(tuoiCon>0){
+            tuoiBo -= 1;
+            tuoiCon -= 1;
+            demNguoc++;
+            if (tuoiBo==2*tuoiCon){
+                demNguocOk = true;
+                break;
+            }
+        }
+        if (demNguocOk==true){
+            cout <<demNguoc<<" nam truoc, tuoi con bang 1/2 tuoi bo";
+        }else{
+            tuoiBo = x;
+            tuoiCon = y;
+            while (tuoiBo != 2*tuoiCon){
+                tuoiBo += 1;
+                tuoiCon += 1;
+                demXuoi++;
+            }
+            cout << "Sau "<<demXuoi<<" nua thi tuoi con bang 1/2 tuoi bo";
+        }
+    }
+}
