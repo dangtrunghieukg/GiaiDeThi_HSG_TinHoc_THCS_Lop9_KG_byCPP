e6134fe5088e561c7d0e32492c10b2a9d35a0dfe
dangtrunghieukg <dangtrunghieuspt@gmail.com>
2023-01-24 09:41:38 +0700

    Xong bai 1

---
+++ 2013_2014_hsgLop9_TinHoc_KG/Bai_1_KG_THCS_Y13_14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int a, b, c, tongLapPhuong, abc;
+int main(){
+    cout << "Nhap so a: "; cin >> a;
+    while(a==0){
+        cout << "Nhap so lai so a (khac 0): "; cin >> a;
+    }
+    cout << "Nhap so b: "; cin >> b;
+    cout << "Nhap so c: "; cin >> c;
+    tongLapPhuong = pow(a,3)+pow(b,3)+pow(c,3);
+    abc = a*100+b*10+c;
+    if(abc == tongLapPhuong){
+        cout << "So " << a << b << c << "=" << a << "^3+" << b <<  "^3+" << c <<  "^3" << endl;
+    }else {
+        cout << "So " << a << b << c << " khong bieu dien duoc abc=" << a << "^3+" << b <<  "^3+" << c <<  "^3" << endl;
+    }
+    return 0;
+}
