09648167a07604882668b4ff30b6200c0d6ae88a
dangtrunghieukg <dangtrunghieuspt@gmail.com>
2023-01-28 09:20:37 +0700

    Vua giai xong bai 1

---
+++ 2006_2007_hsgLop9_TinHoc_KG/Bai_1_KG_THCS_Y06_07.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int N, doCaoMax, Xn=0, Yn=0;
+int main(){
+    cout << "Nhap N = "; cin >> N;
+    doCaoMax = N/2; // division integer
+    cout << "Tong so buoc di: " << N << endl;
+    cout << "Do cao cuc dai: " << doCaoMax << endl;
+    cout << "Toa do cac dinh: ";
+    for(int i=1; i<=N; i++){
+        if(i <= doCaoMax){
+            Xn++;
+            Yn++;
+            cout << "(" << Xn << "," << Yn << ");";
+        }else{
+            Xn++;
+            if(Yn>0) Yn--;
+            cout << "(" << Xn << "," << Yn << ")";
+            (i < N) ? cout <<";" : cout <<""; // short if
+        }
+    }
+    return 0;
+}
