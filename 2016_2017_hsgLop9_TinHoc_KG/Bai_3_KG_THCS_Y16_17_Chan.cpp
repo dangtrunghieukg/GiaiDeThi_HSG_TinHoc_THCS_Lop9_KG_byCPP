d22287df21511f2e1b6784605871cd4e6c500cfb
lamvinhchan <vinhchan74@gmail.com>
2023-01-31 15:39:17 +0700

    giai xong bai 3

---
+++ 2016_2017_hsgLop9_TinHoc_KG/Bai_3_KG_THCS_Y16_17_Chan.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int arrA[100]={0};
+int main(){
+    int N;
+    cout <<"Nhap N: "; cin >>N;
+    cout <<"Nhap day: "<<endl;
+    for(int i=0; i<N; i++){
+        cout<<"A["<<i+1<<"]"<<"="; cin>>arrA[i];
+    }
+
+    int temp;
+    for(int i=0; i<N; i++){
+        for(int j=i; j<N;j++){
+            if(arrA[i]<arrA[j]){
+                temp=arrA[i];
+                arrA[i]=arrA[j];
+                arrA[j]=temp;
+            }
+        }
+    }
+    cout<<"Day sau khi xoa cac phan tu trung nhau: ";
+    for(int i=0; i<N; i++){
+        if(arrA[i]!=arrA[i+1]){
+            cout <<arrA[i]<<" ";
+        }
+    }
+
+return 0;
+}
+
