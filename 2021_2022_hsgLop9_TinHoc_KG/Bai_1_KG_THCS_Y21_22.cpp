ee9a783126f213e3c28a3c357ad36f405f7dde64
dangtrunghieukg <dangtrunghieuspt@gmail.com>
2023-03-23 23:33:20 +0700

    Giải xong bài 1

---
+++ 2021_2022_hsgLop9_TinHoc_KG/Bai_1_KG_THCS_Y21_22.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string strInput = "ABcBA";
+    string strOutput = "";
+    for (int i = strInput.length() - 1; i >= 0; i--) {
+        strOutput += strInput[i];
+    }
+    if (strInput == strOutput) {
+        cout << "Xau " << strInput << " la xau Palindrome" << endl;    
+    } else {
+        cout << "Xau " << strInput << " khong phai xau Palindrome" << endl;    
+    }
+
+    return 0;
+}
