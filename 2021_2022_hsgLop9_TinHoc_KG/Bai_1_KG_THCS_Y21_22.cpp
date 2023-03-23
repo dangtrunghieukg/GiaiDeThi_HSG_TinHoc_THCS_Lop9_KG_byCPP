4ea90500295b9ee9b13cd3fa56c1d9f3a988e8f7
dangtrunghieukg <dangtrunghieuspt@gmail.com>
2023-03-23 23:37:18 +0700

    Xong Baii 1

--- 2021_2022_hsgLop9_TinHoc_KG/Bai_1_KG_THCS_Y21_22.cpp
+++ 2021_2022_hsgLop9_TinHoc_KG/Bai_1_KG_THCS_Y21_22.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
     string strInput = "ABcBA";
+    cout << "Nhap xau: ";
+    getline(cin, strInput);
     string strOutput = "";
     for (int i = strInput.length() - 1; i >= 0; i--) {
         strOutput += strInput[i];
@@ -12,6 +14,5 @@ int main() {
     } else {
         cout << "Xau " << strInput << " khong phai xau Palindrome" << endl;    
     }
-
     return 0;
 }
